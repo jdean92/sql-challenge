28762decd48908e2b7e3ee121fc5553c5db4b396
Chris Birch <you@example.com>
2021-03-14 17:41:43 -0500

    added second qu

--- Instructions/EmployeeSQL/homework.sql
+++ Instructions/EmployeeSQL/homework.sql
@@ -11,3 +11,14 @@ select * from titles;
 select employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary from employees 
        inner join salaries on employees.emp_no = salaries.emp_no order by emp_no;
 
+
+--2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date FROM employees 
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31' order by hire_date;
+
+
+--3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.	
+
+
+
+
