e8bbbf12af1d4681509c650821b751696e45ba78
Chris Birch <you@example.com>
2021-03-15 19:25:56 -0500

    add

--- Instructions/EmployeeSQL/homework.sql
+++ Instructions/EmployeeSQL/homework.sql
@@ -30,3 +30,10 @@ select e.emp_no, e.last_name, e.first_name, d.dept_name from employees e
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
 
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
