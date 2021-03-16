44d8afe38e3d1f5264ebbbe645ebd0c7cd8a5445
Chris Birch <you@example.com>
2021-03-15 19:30:48 -0500

    add4ed q5

--- Instructions/EmployeeSQL/homework.sql
+++ Instructions/EmployeeSQL/homework.sql
@@ -28,7 +28,8 @@ select e.emp_no, e.last_name, e.first_name, d.dept_name from employees e
 	inner join departments d on d.dept_no=de.dept_no order by emp_no;
 
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-
+select first_name, last_name, sex from employees
+where first_name = 'Hercules' and last_name like 'B%';
 
 --6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
