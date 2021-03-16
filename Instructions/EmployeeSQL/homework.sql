7f5b51f89569b952af31c24e42d20fdb17434e93
Chris Birch <you@example.com>
2021-03-15 19:57:48 -0500

    q7

--- Instructions/EmployeeSQL/homework.sql
+++ Instructions/EmployeeSQL/homework.sql
@@ -38,6 +38,9 @@ select e.emp_no, e.last_name, e.first_name, d.dept_name from employees e
 where dept_name='Sales'order by emp_no;
 
 --7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
+select e.emp_no, e.last_name, e.first_name, d.dept_name from employees e
+	inner join dept_emp de on de.emp_no=e.emp_no
+	inner join departments d on d.dept_no=de.dept_no
+where dept_name='Sales' or dept_name='Development' order by emp_no;
 
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
